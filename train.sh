0ab5218796a0417435db8bc1d8dbc7d4b38e8024
Ugnius Malukas <ugnius.malukas@matom.ai>
2024-10-11 10:20:57 +0300

    Add train.sh and train_sagemaker.py

---
+++ train.sh
@@ -0,0 +1,2 @@
+python train_sagemaker.py --cfg cfg/training/yolov7-tiny.yaml --hyp data/hyp.scratch.tiny.yaml --name exp --epochs 5 --batch-size 3 --data dataset/data.yaml --img 640
+#python train_sagemaker.py --cfg cfg/training/yolov7-tiny.yaml --hyp data/hyp.scratch.tiny.yaml --name exp --epochs 5 --batch-size 3 --data dataset/data.yaml --img 640 --weights dataset/model.pt
