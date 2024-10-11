1fbb5460dfe2279da313c77d2adcdccc352d96df
Ugnius Malukas <ugnius.malukas@matom.ai>
2024-10-11 13:07:50 +0300

    Saving changes

--- train_sagemaker.py
+++ train_sagemaker.py
@@ -464,10 +464,10 @@ def train(hyp, opt, device, tb_writer=None):
                 torch.save(ckpt, last)
                 if best_fitness == fi:
                     torch.save(ckpt, best)
-                    if opt.best_pt_dir:
-                        best_pt_dir = Path(opt.best_pt_dir)
-                        best_pt_dir.mkdir(parents=True, exist_ok=True)
-                        torch.save(ckpt['model'], best_pt_dir / 'model.pt')
+                    # if opt.best_pt_dir:
+                    #     best_pt_dir = Path(opt.best_pt_dir)
+                    #     best_pt_dir.mkdir(parents=True, exist_ok=True)
+                    #     torch.save(ckpt['model'], best_pt_dir / 'model.pt')
                 if (best_fitness == fi) and (epoch >= 200):
                     torch.save(ckpt, wdir / 'best_{:03d}.pt'.format(epoch))
                 if epoch == 0:
